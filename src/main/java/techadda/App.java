c548026bdcd2180080339e90d4616d5974bbbafc
vikas108828 <vikas108828@gmail.com>
2020-04-28 17:27:57 +0000

    intial commit

---
+++ src/main/java/techadda/App.java
@@ -0,0 +1,13 @@
+package techadda;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
